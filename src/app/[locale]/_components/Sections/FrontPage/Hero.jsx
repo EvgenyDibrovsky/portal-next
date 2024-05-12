e16bbd3c639f338ddbceeaa354b1cf96a5963b88
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2024-05-12 22:42:20 +0200

    commit

--- src/app/[locale]/_components/Sections/FrontPage/Hero.jsx
+++ src/app/[locale]/_components/Sections/FrontPage/Hero.jsx
@@ -4,7 +4,8 @@
 import Category from "../../../_components/MainCategory/MainCategoryList";
 export default function Hero() {
   return (
-    <section className="flex w-full h-[calc(100dvh-3.5rem)]">
+    <section className="flex flex-col justify-around w-full h-[calc(100dvh-3.5rem)]">
+      <div></div>
       <div className="container">
         <Category />
       </div>
