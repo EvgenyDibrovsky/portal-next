ab201ab839475d4dbe70c832037fe69b6cec0f07
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2024-05-14 23:07:41 +0200

    commit

--- src/app/[locale]/_components/Bloggers/Bloggers.jsx
+++ src/app/[locale]/_components/Bloggers/Bloggers.jsx
@@ -2,28 +2,42 @@ import Image from "next/image";
 import Link from "next/link";
 export default function Bloggers() {
   return (
-    <section className="py-[4rem] bg-neutral-200">
+    <section className="py-[4rem] bg-neutral-100">
       <div className="container">
-        <ul className="flex gap-8">
-          <li className="group">
-            <Link href="#" target="_blank" className="flex flex-col items-center justify-center gap-4">
-              <Image src="/images/demo/demo-persone.jpg" alt="Example Image" width={70} height={70} className="rounded-full duration-300 group-hover:scale-110" />
-              <span className="duration-300 group-hover:text-sky-600">David Scott</span>
-            </Link>
-          </li>
-          <li className="group">
-            <Link href="#" target="_blank" className="flex flex-col items-center justify-center gap-4">
-              <Image src="/images/demo/demo-persone.jpg" alt="Example Image" width={70} height={70} className="rounded-full duration-300 group-hover:scale-110" />
-              <span className="duration-300 group-hover:text-sky-600">David Scott</span>
-            </Link>
-          </li>
-          <li className="group">
-            <Link href="#" target="_blank" className="flex flex-col items-center justify-center gap-4">
-              <Image src="/images/demo/demo-persone.jpg" alt="Example Image" width={70} height={70} className="rounded-full duration-300 group-hover:scale-110" />
-              <span className="duration-300 group-hover:text-sky-600">David Scott</span>
-            </Link>
-          </li>
-        </ul>
+        <div className="flex">
+          <div className="w-4/12"></div>
+          <div className="w-8/12">
+            <ul className="grid grid-cols-3 gap-2">
+              <li className="group">
+                <Link href="#" target="_blank" className="flex items-start gap-4">
+                  <Image src="/images/demo/demo-persone.jpg" alt="Example Image" width={100} height={100} className="rounded-full duration-300 group-hover:scale-110 shadow-md" />
+                  <div className="flex flex-col justify-between gap-2 h-full">
+                    <h5 className="duration-300 group-hover:text-sky-600 text-[1.4rem] font-medium">David Scott</h5>
+                    <p className="text-[1.2rem]">Актуальные новости Польши</p>
+                  </div>
+                </Link>
+              </li>
+              <li className="group">
+                <Link href="#" target="_blank" className="flex items-start gap-4">
+                  <Image src="/images/demo/demo-persone.jpg" alt="Example Image" width={100} height={100} className="rounded-full duration-300 group-hover:scale-110 shadow-md" />
+                  <div className="flex flex-col justify-between gap-2 h-full">
+                    <h5 className="duration-300 group-hover:text-sky-600 text-[1.4rem] font-medium">David Scott</h5>
+                    <p className="text-[1.2rem]">Актуальные новости Польши</p>
+                  </div>
+                </Link>
+              </li>
+              <li className="group">
+                <Link href="#" target="_blank" className="flex items-start gap-4">
+                  <Image src="/images/demo/demo-persone.jpg" alt="Example Image" width={100} height={100} className="rounded-full duration-300 group-hover:scale-110 shadow-md" />
+                  <div className="flex flex-col justify-between gap-2 h-full">
+                    <h5 className="duration-300 group-hover:text-sky-600 text-[1.4rem] font-medium">David Scott</h5>
+                    <p className="text-[1.2rem]">Новости Украины</p>
+                  </div>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </section>
   );
