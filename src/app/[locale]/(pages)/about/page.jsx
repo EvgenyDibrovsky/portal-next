40746722998134f96122cd24ed1f1371fa689745
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2024-05-15 20:41:43 +0200

    commit

--- src/app/[locale]/(pages)/about/page.jsx
+++ src/app/[locale]/(pages)/about/page.jsx
@@ -1,4 +1,8 @@
 import Titles from "../../_components/PageSettings/Titles";
+import Image from "next/image";
+import { Link } from "../../../../navigation";
+import { BsBox } from "react-icons/bs";
+
 // import { useTranslations } from "next-intl";
 
 export default function About() {
@@ -9,6 +13,77 @@ export default function About() {
       <div className="container">
         <Titles title="О нас" subTitle="Подзаголовок" />
         <div className="flex w-full max-w-sm overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800"></div>
+        <div className="flex gap-[2rem]">
+          <div className="w-6/12">
+            <div className="relative flex items-center justify-center bg-transparent w-full h-full mb-5 ">
+              <Image src="/images/demo/team.jpg" alt="Example Image" layout="fill" className="object-cover object-top" />
+            </div>
+          </div>
+          <div className="w-6/12">
+            <div className="flex flex-col justify-between">
+              <h2 className="mb-2 text-black font-medium text-[1.6rem] relative pb-2 after:absolute after:bottom-0 after:left-0 after:content-[''] after:block after:w-[4rem] after:h-[2px] after:bg-gradient-to-r after:from-sky-600 from-10% after:via-sky-500 via-30% after:to-sky-300 to-80%">
+                О нас
+              </h2>
+
+              <p className="mb-[1rem]">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi numquam possimus aspernatur, voluptatum quis cum vitae, saepe pariatur amet eveniet expedita eaque magni. Sint voluptates
+                earum animi, laborum quasi ducimus.
+                <br />
+                <br /> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi numquam possimus aspernatur, voluptatum quis cum vitae, saepe pariatur amet eveniet expedita eaque magni. Sint
+                voluptates earum animi, laborum quasi ducimus.
+              </p>
+              <ul className="grid grid-cols-2 gap-x-[1rem] list-disc	 list-inside marker:text-sky-600 marker:text-[1.2rem]">
+                <li className="">Позиция 1</li>
+                <li className="">Позиция 2</li>
+                <li className="">Позиция 3</li>
+                <li className="">Позиция 4</li>
+                <li className="">Позиция 5</li>
+                <li className="">Позиция 6</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <ul className="grid grid-cols-8 py-[4rem]">
+          <li className="flex justify-center items-center flex-col py-[2rem] px-[1rem] text-neutral-300">
+            <BsBox className="mx-auto w-[4rem] h-[4rem] mb-4 border-2 p-4 rounded-full" />
+            Работа
+          </li>
+
+          <li className="flex justify-center items-center flex-col py-[2rem] px-[1rem] text-neutral-300">
+            <BsBox className="mx-auto w-[4rem] h-[4rem] mb-4 border-2 p-4 rounded-full" />
+            Недвижимость
+          </li>
+
+          <li className="flex justify-center items-center flex-col py-[2rem] px-[1rem] text-neutral-300">
+            <BsBox className="mx-auto w-[4rem] h-[4rem] mb-4 border-2 p-4 rounded-full" />
+            Финансы
+          </li>
+
+          <li className="flex justify-center items-center flex-col py-[2rem] px-[1rem] text-neutral-300">
+            <BsBox className="mx-auto w-[4rem] h-[4rem] mb-4 border-2 p-4 rounded-full" />
+            Право
+          </li>
+
+          <li className="flex justify-center items-center flex-col py-[2rem] px-[1rem] text-neutral-300">
+            <BsBox className="mx-auto w-[4rem] h-[4rem] mb-4 border-2 p-4 rounded-full" />
+            Бизнес
+          </li>
+
+          <li className="flex justify-center items-center flex-col py-[2rem] px-[1rem] text-neutral-300">
+            <BsBox className="mx-auto w-[4rem] h-[4rem] mb-4 border-2 p-4 rounded-full" />
+            Образование
+          </li>
+
+          <li className="flex justify-center items-center flex-col py-[2rem] px-[1rem] text-neutral-300">
+            <BsBox className="mx-auto w-[4rem] h-[4rem] mb-4 border-2 p-4 rounded-full" />
+            Блоги
+          </li>
+
+          <li className="flex justify-center items-center flex-col py-[2rem] px-[1rem] text-sky-600">
+            <BsBox className="mx-auto w-[4rem] h-[4rem] mb-4 border-2 border-sky-600 p-4 rounded-full" />
+            Новости
+          </li>
+        </ul>
       </div>
     </section>
   );
--- src/app/[locale]/_components/Blog/BlogSwiper/BlogSwiper.jsx
+++ src/app/[locale]/_components/Blog/BlogSwiper/BlogSwiper.jsx
@@ -17,39 +17,41 @@ export default function BlogSwiper() {
         spaceBetween={50}
         slidesPerView={1}
         navigation={{
-          nextEl: ".swiper-button-next",
           prevEl: ".swiper-button-prev",
+          nextEl: ".swiper-button-next",
         }}
-        autoplay={{ delay: 5000 }}
+        autoplay={{ delay: 500000 }}
         pagination={{ clickable: true }}
-        className="swiper-blog relative"
+        className="swiper-blog"
       >
         <SwiperSlide className="swiper-slide ">
-          <div className="flex w-full h-[30rem] relative">
+          <div className="flex w-full h-[30rem]">
             <Image src="/images/home_slider/slide_1.jpg" alt="Example Image" layout="fill" className="object-cover" />
-            <div className="w-10/12 mt-[2rem] h-fit px-[2rem] py-[1rem] bg-black/50 relative z-20">
+            <div className="w-10/12 mt-[2rem] h-fit px-[2rem] py-[1rem] bg-black/50 relative z-20 shadow-md ">
               <h2 className="mb-[1rem] text-white text-[1.6rem] font-medium">Nowoczesna aranżacja Twojego ogrodu</h2>
               <p className=" text-white text-[1.2rem] ">Marka GiardDesign to wieloletnie doświadczenie i wysoka estetyka realizacji.</p>
             </div>
-            <div className="absolute bottom-0 left-0 py-1 px-2 bg-black/50 text-white">09.05.2024</div>
+            <div className="absolute bottom-0 left-0 py-1 px-2 bg-black/50 text-white text-[0.7rem]">09.05.2024</div>
           </div>
         </SwiperSlide>
         <SwiperSlide className="swiper-slide">
           <div className="flex w-full h-[30rem] relative">
             <Image src="/images/home_slider/slide_2.jpg" alt="Example Image" layout="fill" className="object-cover" />
-            <div className="w-10/12 mt-[2rem] h-fit px-[2rem] py-[1rem] bg-black/50 relative z-20">
+            <div className="w-10/12 mt-[2rem] h-fit px-[2rem] py-[1rem] bg-black/50 relative z-20 shadow-md">
               <h2 className="mb-[1rem] text-white text-[1.6rem] font-medium">Nowoczesna aranżacja Twojego ogrodu</h2>
               <p className=" text-white text-[1.2rem] ">
                 Marka GiardDesign to wieloletnie doświadczenie i wysoka estetyka realizacji.Marka GiardDesign to wieloletnie doświadczenie i wysoka estetyка realizacji.Marka GiardDesign to wieloletnie
                 doświadczenie i wysoka estetyka realizacji.
               </p>
             </div>
-            <div className="absolute bottom-0 left-0 py-1 px-2 bg-black/50 text-white">10.05.2024</div>
+            <div className="absolute bottom-0 left-0 py-1 px-2 bg-black/50 text-white text-[0.7rem]">10.05.2024</div>
           </div>
         </SwiperSlide>
-        <div className="w-[10rem] h-auto z-20 absolute bottom-0 right-0 bg-white">
-          <div className="swiper-button-prev"></div>
-          <div className="swiper-button-next"></div>
+        <div className="absolute bottom-0 w-[6.5rem] right-0 z-10 ">
+          <div className="relative flex items-center justify-center z-50 h-[3.5rem]">
+            <div className="swiper-button-prev"></div>
+            <div className="swiper-button-next"></div>
+          </div>
         </div>
       </Swiper>
     </div>
