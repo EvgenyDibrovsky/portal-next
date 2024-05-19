e37921f97248bea80186f62123ac8ddf09fa14e5
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2024-05-19 23:18:17 +0200

    Update page.jsx

--- src/app/[locale]/(pages)/contacts/page.jsx
+++ src/app/[locale]/(pages)/contacts/page.jsx
@@ -5,8 +5,12 @@ export default function Contacts() {
     <section className="h-[100dvh] py-[2.5rem]">
       <div className="container">
         <Titles title="Контакты" subTitle="Подзаголовок" />
-
-        <div className="w-8/12 mx-auto">контактная форма</div>
+        <div className="flex gap-[1rem]">
+          <div className="w-4/12 mx-auto">контактная форма</div>
+          <div className="w-8/12 mx-auto">
+            <form action=""></form>
+          </div>
+        </div>
       </div>
     </section>
   );
