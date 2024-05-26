d287040b5923056c226cdabeaa35141ea2afd9bd
EvgenyDibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2024-05-26 22:12:22 +0200

    commit

--- src/app/[locale]/(pages)/contacts/page.jsx
+++ src/app/[locale]/(pages)/contacts/page.jsx
@@ -1,13 +1,68 @@
 import Titles from "../../_components/PageSettings/TitlePage/Titles";
 import ContactFormContactPage from "../../_components/ContactForms/ContactPage/ContactFormContactPage";
+import { BsTelephone, BsEnvelope, BsFillPinMapFill, BsFacebook, BsTelegram, BsLinkedin } from "react-icons/bs";
+
 export default function Contacts() {
   return (
     <section className="min-h-[calc(100dvh-14rem)] py-[2.5rem]">
       <div className="container mx-auto">
         <Titles title="Контакты" subTitle="Подзаголовок" />
-        <div className="flex justify-center gap-4">
-          <div className="w-5/12">
+        <div className="flex justify-between gap-[2rem]">
+          <div className="w-6/12">
+            <h2 className="text-[1.2rem] font-semibold mb-[1rem]">Cвязатся с нами</h2>
             <ContactFormContactPage />
+          </div>
+          <div className="w-6/12">
+            <div className="flex flex-col justify-between h-full">
+              <div>
+                <h2 className="text-[1.2rem] font-semibold mb-[1rem]">Детальная информация</h2>
+                <div className="flex flex-col gap-[2rem]">
+                  <div className="flex items-start gap-[1.5rem]">
+                    <div>
+                      <BsTelephone className="text-sky-600 text-[1.3rem] mt-[0.5rem]" />
+                    </div>
+                    <div className="flex flex-col gap-[1rem]">
+                      <h3 className="font-bold text-[1.1rem]">Phone</h3>
+                      <div>+48 537 450 501</div>
+                      <div>+48 537 450 501</div>
+                    </div>
+                  </div>
+                  <div className="flex items-start gap-[1.5rem]">
+                    <div>
+                      <BsEnvelope className="text-sky-600 text-[1.3rem] mt-[0.5rem]" />
+                    </div>
+                    <div className="flex flex-col gap-[1rem]">
+                      <h3 className="font-bold text-[1.1rem]">Email</h3>
+                      <div>edweb@contact.com</div>
+                      <div>edweb@contact.com</div>
+                    </div>
+                  </div>
+                  <div className="flex items-start gap-[1.5rem]">
+                    <div>
+                      <BsFillPinMapFill className="text-sky-600 text-[1.3rem] mt-[0.5rem]" />
+                    </div>
+                    <div className="flex flex-col gap-[1rem]">
+                      <h3 className="font-bold text-[1.1rem]">Location</h3>
+                      <div>4517 Washington Ave. 32 King Street, Melbourne Manchester, Road USA</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <h2 className="text-[1.2rem] font-semibold mb-[1rem]">Подписывайтесь на нас</h2>
+                <ul className="flex items-center gap-[1.5rem]">
+                  <li>
+                    <BsFacebook className="text-[2rem] rounded-full text-sky-600" />
+                  </li>
+                  <li>
+                    <BsTelegram className="text-[2rem] rounded-full text-sky-600" />
+                  </li>
+                  <li>
+                    <BsLinkedin className="text-[2rem] rounded-full text-sky-600" />
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/app/[locale]/_components/Bussines/HomePageBussinesCategory/HomePageBussinesCategoryListItem.jsx
+++ src/app/[locale]/_components/Bussines/HomePageBussinesCategory/HomePageBussinesCategoryListItem.jsx
@@ -1,132 +1,158 @@
 // import Image from "next/image";
 import { Link } from "../../../../../navigation";
+import { BsArrowRightShort } from "react-icons/bs";
 
 export default function HomePageBussinesCategoryListItem() {
   return (
     <ul className="grid grid-cols-2 gap-x-[1.5rem] gap-y-[0.5rem]">
       <li>
-        <Link href="#" className="text-sky-600 text-[1rem] duration-300 hover:underline">
-          AGD i RTV
+        <Link href="#" className="relative inline-flex items-center text-black text-[1rem] duration-300 hover:text-sky-600 group">
+          <span className="mr-2">AGD i RTV</span>
+          <BsArrowRightShort className="-translate-x-3 opacity-0 duration-300 absolute right-0 top-1/2 transform -translate-y-1/2 group-hover:opacity-100  group-hover:translate-x-3 " />
         </Link>
       </li>
       <li>
-        <Link href="#" className="text-sky-600 text-[1rem] duration-300 hover:underline">
-          Budownictwo
+        <Link href="#" className="relative inline-flex items-center text-black text-[1rem] duration-300 hover:text-sky-600 group ">
+          <span className="mr-2">Budownictwo</span>
+          <BsArrowRightShort className="-translate-x-3 opacity-0 duration-300 absolute right-0 top-1/2 transform -translate-y-1/2 group-hover:opacity-100  group-hover:translate-x-3 " />
         </Link>
       </li>
       <li>
-        <Link href="#" className="text-sky-600 text-[1rem] duration-300 hover:underline">
-          Chemia, oleje, paliwa
+        <Link href="#" className="relative inline-flex items-center text-black text-[1rem] duration-300 hover:text-sky-600 group ">
+          <span className="mr-2">Chemia, oleje, paliwa</span>
+          <BsArrowRightShort className="-translate-x-3 opacity-0 duration-300 absolute right-0 top-1/2 transform -translate-y-1/2 group-hover:opacity-100  group-hover:translate-x-3 " />
         </Link>
       </li>
       <li>
-        <Link href="#" className="text-sky-600 text-[1rem] duration-300 hover:underline">
-          Edukacja, doradztwo
+        <Link href="#" className="relative inline-flex items-center text-black text-[1rem] duration-300 hover:text-sky-600 group ">
+          <span className="mr-2">Edukacja, doradztwo</span>
+          <BsArrowRightShort className="-translate-x-3 opacity-0 duration-300 absolute right-0 top-1/2 transform -translate-y-1/2 group-hover:opacity-100  group-hover:translate-x-3 " />
         </Link>
       </li>
       <li>
-        <Link href="#" className="text-sky-600 text-[1rem] duration-300 hover:underline">
-          Ekologia, środowisko
+        <Link href="#" className="relative inline-flex items-center text-black text-[1rem] duration-300 hover:text-sky-600 group ">
+          <span className="mr-2">Ekologia, środowisko</span>
+          <BsArrowRightShort className="-translate-x-3 opacity-0 duration-300 absolute right-0 top-1/2 transform -translate-y-1/2 group-hover:opacity-100  group-hover:translate-x-3 " />
         </Link>
       </li>
       <li>
-        <Link href="#" className="text-sky-600 text-[1rem] duration-300 hover:underline">
-          Elektronika
+        <Link href="#" className="relative inline-flex items-center text-black text-[1rem] duration-300 hover:text-sky-600 group ">
+          <span className="mr-2">Elektronika</span>
+          <BsArrowRightShort className="-translate-x-3 opacity-0 duration-300 absolute right-0 top-1/2 transform -translate-y-1/2 group-hover:opacity-100  group-hover:translate-x-3 " />
         </Link>
       </li>
       <li>
-        <Link href="#" className="text-sky-600 text-[1rem] duration-300 hover:underline">
-          Finanse, ubezpieczenia
+        <Link href="#" className="relative inline-flex items-center text-black text-[1rem] duration-300 hover:text-sky-600 group ">
+          <span className="mr-2">Finanse, ubezpieczenia</span>
+          <BsArrowRightShort className="-translate-x-3 opacity-0 duration-300 absolute right-0 top-1/2 transform -translate-y-1/2 group-hover:opacity-100  group-hover:translate-x-3 " />
         </Link>
       </li>
       <li>
-        <Link href="#" className="text-sky-600 text-[1rem] duration-300 hover:underline">
-          Gastronomia
+        <Link href="#" className="relative inline-flex items-center text-black text-[1rem] duration-300 hover:text-sky-600 group ">
+          <span className="mr-2">Gastronomia</span>
+          <BsArrowRightShort className="-translate-x-3 opacity-0 duration-300 absolute right-0 top-1/2 transform -translate-y-1/2 group-hover:opacity-100  group-hover:translate-x-3 " />
         </Link>
       </li>
       <li>
-        <Link href="#" className="text-sky-600 text-[1rem] duration-300 hover:underline">
-          Komputery, Internet
+        <Link href="#" className="relative inline-flex items-center text-black text-[1rem] duration-300 hover:text-sky-600 group ">
+          <span className="mr-2">Komputery, Internet</span>
+          <BsArrowRightShort className="-translate-x-3 opacity-0 duration-300 absolute right-0 top-1/2 transform -translate-y-1/2 group-hover:opacity-100  group-hover:translate-x-3 " />
         </Link>
       </li>
       <li>
-        <Link href="#" className="text-sky-600 text-[1rem] duration-300 hover:underline">
-          Księgowość, biuro
+        <Link href="#" className="relative inline-flex items-center text-black text-[1rem] duration-300 hover:text-sky-600 group ">
+          <span className="mr-2">Księgowość, biuro</span>
+          <BsArrowRightShort className="-translate-x-3 opacity-0 duration-300 absolute right-0 top-1/2 transform -translate-y-1/2 group-hover:opacity-100  group-hover:translate-x-3 " />
         </Link>
       </li>
       <li>
-        <Link href="#" className="text-sky-600 text-[1rem] duration-300 hover:underline">
-          Kultura, sztuka
+        <Link href="#" className="relative inline-flex items-center text-black text-[1rem] duration-300 hover:text-sky-600 group ">
+          <span className="mr-2">Kultura, sztuka</span>
+          <BsArrowRightShort className="-translate-x-3 opacity-0 duration-300 absolute right-0 top-1/2 transform -translate-y-1/2 group-hover:opacity-100  group-hover:translate-x-3 " />
         </Link>
       </li>
       <li>
-        <Link href="#" className="text-sky-600 text-[1rem] duration-300 hover:underline">
-          Marketing, reklama
+        <Link href="#" className="relative inline-flex items-center text-black text-[1rem] duration-300 hover:text-sky-600 group ">
+          <span className="mr-2">Marketing, reklama</span>
+          <BsArrowRightShort className="-translate-x-3 opacity-0 duration-300 absolute right-0 top-1/2 transform -translate-y-1/2 group-hover:opacity-100  group-hover:translate-x-3 " />
         </Link>
       </li>
       <li>
-        <Link href="#" className="text-sky-600 text-[1rem] duration-300 hover:underline">
-          Meble, wyposażenie wnętrz
+        <Link href="#" className="relative inline-flex items-center text-black text-[1rem] duration-300 hover:text-sky-600 group ">
+          <span className="mr-2">Meble, wyposażenie wnętrz</span>
+          <BsArrowRightShort className="-translate-x-3 opacity-0 duration-300 absolute right-0 top-1/2 transform -translate-y-1/2 group-hover:opacity-100  group-hover:translate-x-3 " />
         </Link>
       </li>
       <li>
-        <Link href="#" className="text-sky-600 text-[1rem] duration-300 hover:underline">
-          Medycyna, farmacja
+        <Link href="#" className="relative inline-flex items-center text-black text-[1rem] duration-300 hover:text-sky-600 group ">
+          <span className="mr-2">Medycyna, farmacja</span>
+          <BsArrowRightShort className="-translate-x-3 opacity-0 duration-300 absolute right-0 top-1/2 transform -translate-y-1/2 group-hover:opacity-100  group-hover:translate-x-3 " />
         </Link>
       </li>
       <li>
-        <Link href="#" className="text-sky-600 text-[1rem] duration-300 hover:underline">
-          Motoryzacja
+        <Link href="#" className="relative inline-flex items-center text-black text-[1rem] duration-300 hover:text-sky-600 group ">
+          <span className="mr-2">Motoryzacja</span>
+          <BsArrowRightShort className="-translate-x-3 opacity-0 duration-300 absolute right-0 top-1/2 transform -translate-y-1/2 group-hover:opacity-100  group-hover:translate-x-3 " />
         </Link>
       </li>
       <li>
-        <Link href="#" className="text-sky-600 text-[1rem] duration-300 hover:underline">
-          Nieruchomości
+        <Link href="#" className="relative inline-flex items-center text-black text-[1rem] duration-300 hover:text-sky-600 group ">
+          <span className="mr-2">Nieruchomości</span>
+          <BsArrowRightShort className="-translate-x-3 opacity-0 duration-300 absolute right-0 top-1/2 transform -translate-y-1/2 group-hover:opacity-100  group-hover:translate-x-3 " />
         </Link>
       </li>
       <li>
-        <Link href="#" className="text-sky-600 text-[1rem] duration-300 hover:underline">
-          Odzież, obuwie, tekstylia
+        <Link href="#" className="relative inline-flex items-center text-black text-[1rem] duration-300 hover:text-sky-600 group ">
+          <span className="mr-2">Odzież, obuwie, tekstylia</span>
+          <BsArrowRightShort className="-translate-x-3 opacity-0 duration-300 absolute right-0 top-1/2 transform -translate-y-1/2 group-hover:opacity-100  group-hover:translate-x-3 " />
         </Link>
       </li>
       <li>
-        <Link href="#" className="text-sky-600 text-[1rem] duration-300 hover:underline">
-          Poligrafia, druk
+        <Link href="#" className="relative inline-flex items-center text-black text-[1rem] duration-300 hover:text-sky-600 group ">
+          <span className="mr-2">Poligrafia, druk</span>
+          <BsArrowRightShort className="-translate-x-3 opacity-0 duration-300 absolute right-0 top-1/2 transform -translate-y-1/2 group-hover:opacity-100  group-hover:translate-x-3 " />
         </Link>
       </li>
       <li>
-        <Link href="#" className="text-sky-600 text-[1rem] duration-300 hover:underline">
-          Przemysł, automatyzacja
+        <Link href="#" className="relative inline-flex items-center text-black text-[1rem] duration-300 hover:text-sky-600 group ">
+          <span className="mr-2">Przemysł, automatyzacja</span>
+          <BsArrowRightShort className="-translate-x-3 opacity-0 duration-300 absolute right-0 top-1/2 transform -translate-y-1/2 group-hover:opacity-100  group-hover:translate-x-3 " />
         </Link>
       </li>
       <li>
-        <Link href="#" className="text-sky-600 text-[1rem] duration-300 hover:underline">
-          Rolnictwo, żywność
+        <Link href="#" className="relative inline-flex items-center text-black text-[1rem] duration-300 hover:text-sky-600 group ">
+          <span className="mr-2">Rolnictwo, żywność</span>
+          <BsArrowRightShort className="-translate-x-3 opacity-0 duration-300 absolute right-0 top-1/2 transform -translate-y-1/2 group-hover:opacity-100  group-hover:translate-x-3 " />
         </Link>
       </li>
       <li>
-        <Link href="#" className="text-sky-600 text-[1rem] duration-300 hover:underline">
-          Rozrywka, rekreacja, sport
+        <Link href="#" className="relative inline-flex items-center text-black text-[1rem] duration-300 hover:text-sky-600 group ">
+          <span className="mr-2">Rozrywka, rekreacja, sport</span>
+          <BsArrowRightShort className="-translate-x-3 opacity-0 duration-300 absolute right-0 top-1/2 transform -translate-y-1/2 group-hover:opacity-100  group-hover:translate-x-3 " />
         </Link>
       </li>
       <li>
-        <Link href="#" className="text-sky-600 text-[1rem] duration-300 hover:underline">
-          Transport, komunikacja
+        <Link href="#" className="relative inline-flex items-center text-black text-[1rem] duration-300 hover:text-sky-600 group ">
+          <span className="mr-2">Transport, komunikacja</span>
+          <BsArrowRightShort className="-translate-x-3 opacity-0 duration-300 absolute right-0 top-1/2 transform -translate-y-1/2 group-hover:opacity-100  group-hover:translate-x-3 " />
         </Link>
       </li>
       <li>
-        <Link href="#" className="text-sky-600 text-[1rem] duration-300 hover:underline">
-          Turystyka
+        <Link href="#" className="relative inline-flex items-center text-black text-[1rem] duration-300 hover:text-sky-600 group ">
+          <span className="mr-2">Turystyka</span>
+          <BsArrowRightShort className="-translate-x-3 opacity-0 duration-300 absolute right-0 top-1/2 transform -translate-y-1/2 group-hover:opacity-100  group-hover:translate-x-3 " />
         </Link>
       </li>
       <li>
-        <Link href="#" className="text-sky-600 text-[1rem] duration-300 hover:underline">
-          Zdrowie, uroda
+        <Link href="#" className="relative inline-flex items-center text-black text-[1rem] duration-300 hover:text-sky-600 group ">
+          <span className="mr-2">Zdrowie, uroda</span>
+          <BsArrowRightShort className="-translate-x-3 opacity-0 duration-300 absolute right-0 top-1/2 transform -translate-y-1/2 group-hover:opacity-100  group-hover:translate-x-3 " />
         </Link>
       </li>
       <li>
-        <Link href="#" className="text-sky-600 text-[1rem] duration-300 hover:underline">
-          Inne
+        <Link href="#" className="relative inline-flex items-center text-black text-[1rem] duration-300 hover:text-sky-600 group ">
+          <span className="mr-2">Inne</span>
+          <BsArrowRightShort className="-translate-x-3 opacity-0 duration-300 absolute right-0 top-1/2 transform -translate-y-1/2 group-hover:opacity-100  group-hover:translate-x-3 " />
         </Link>
       </li>
     </ul>
